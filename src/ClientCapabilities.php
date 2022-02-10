2729ed1caa8456752f6480efc1d670565307b744
Damjan Cvetko <damjan.cvetko@monotek.net>
2022-02-10 06:17:51 +0100

    Re-introducing X fields described in https://github.com/sourcegraph/language-server-protocol

--- src/ClientCapabilities.php
+++ src/ClientCapabilities.php
@@ -43,6 +43,27 @@ class ClientCapabilities
     public $experimental;
 
     /**
+     * The client supports workspace/xfiles requests
+     *
+     * @var bool|null
+     */
+    public $xfilesProvider;
+
+    /**
+     * The client supports textDocument/xcontent requests
+     *
+     * @var bool|null
+     */
+    public $xcontentProvider;
+
+    /**
+     * The client supports xcache/* requests
+     *
+     * @var bool|null
+     */
+    public $xcacheProvider;
+
+    /**
      * Undocumented function
      *
      * @param ClientCapabilitiesWorkspace|null $workspace
@@ -50,18 +71,27 @@ class ClientCapabilities
      * @param ClientCapabilitiesWindow|null $window
      * @param ClientCapabilitiesGeneral|null $general
      * @param mixed|null $experimental
+     * @param bool|null $xfilesProvider
+     * @param bool|null $xcontentProvider
+     * @param bool|null $xcacheProvider
      */
     public function __construct(
         ClientCapabilitiesWorkspace $workspace = null,
         TextDocumentClientCapabilities $textDocument = null,
         ClientCapabilitiesWindow $window = null,
         ClientCapabilitiesGeneral $general = null,
-        $experimental = null
+        $experimental = null,
+        bool $xfilesProvider = null,
+        bool $xcontentProvider = null,
+        bool $xcacheProvider = null
     ) {
         $this->workspace = $workspace;
         $this->textDocument = $textDocument;
         $this->window = $window;
         $this->general = $general;
         $this->experimental = $experimental;
+        $this->xfilesProvider = $xfilesProvider;
+        $this->xcontentProvider = $xcontentProvider;
+        $this->xcacheProvider = $xcacheProvider;
     }
 }
